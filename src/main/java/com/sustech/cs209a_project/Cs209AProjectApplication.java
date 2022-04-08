b7d02b11eb4a2ad13b9c31db4527566621d9f12e
xbdeng <2327619908@qq.com>
2022-04-08 22:21:01 +0800

    initial

---
+++ src/main/java/com/sustech/cs209a_project/Cs209AProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sustech.cs209a_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs209AProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs209AProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sustech/cs209a_project/Cs209AProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustech.cs209a_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs209AProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
