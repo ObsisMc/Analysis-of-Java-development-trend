c385f452a957e1dd626619af013494e2225e5ec8
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-07 19:03:47 +0800

    install echarts

--- front_end/src/components/HelloWorld.vue
+++ front_end/src/components/HelloWorld.vue
@@ -1,95 +1,27 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <test ref="test"></test>
   </div>
 </template>
 
 <script>
+import test from "./test";
+
 export default {
   name: 'HelloWorld',
-  data () {
+
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      name: '张雪',
+      xData: ['2020-02', '2020-03', '2020-04', '2020-05'],
+      yData: [30, 132, 80, 134],
     }
+  },
+  components: {
+    test
+  },
+  mounted() {
+    this.$refs.test.initChart(this.name, this.xData, this.yData);
   }
 }
 </script>
@@ -99,14 +31,17 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
