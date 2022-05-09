1a5a93161e46e1c3a49c97b4a1908374e25f0a6d
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-09 15:21:03 +0800

    add method to get relationship

--- src/main/java/com/sustech/cs209a_project/Utils/JsonIO.java
+++ src/main/java/com/sustech/cs209a_project/Utils/JsonIO.java
@@ -2,15 +2,43 @@ package com.sustech.cs209a_project.Utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class JsonIO {
+    public static void main(String[] args) {
+        JSONArray json4 = readJSONArray("repos10000to1000000.json");
+        JSONArray json1 = readJSONArray("repos10to100.json");
+        JSONArray json2 = readJSONArray("repos100to1000.json");
+        JSONArray json3 = readJSONArray("repos1000to10000.json");
+        System.out.printf("%d, %d, %d, %d\n", json1.size(), json2.size(), json3.size(),json4.size());
+        JSONArray jsonTotal = new JSONArray();
+        jsonTotal.addAll(json1);
+        jsonTotal.addAll(json2);
+        jsonTotal.addAll(json3);
+        jsonTotal.addAll(json4);
+        saveJSONArray(jsonTotal,"jsonTotal.json");
+
+    }
+
     public static void saveJSONArray(JSONArray jsonArray, String path) {
         saveString(JSON.toJSONString(jsonArray), path);
+    }
+    public static void saveJSON(JSONObject json, String path) {
+        saveString(JSON.toJSONString(json), path);
+    }
+
+    public static JSONArray readJSONArray(String path) {
+        try (BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream(path)))) {
+            String jsonStr = bfr.readLine();
+            return JSON.parseArray(jsonStr);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void saveString(String content, String path) {
@@ -22,4 +50,7 @@ public class JsonIO {
             e.printStackTrace();
         }
     }
+
+
+
 }
--- src/main/java/com/sustech/cs209a_project/Utils/RepoSearcher.java
+++ src/main/java/com/sustech/cs209a_project/Utils/RepoSearcher.java
@@ -3,15 +3,36 @@ package com.sustech.cs209a_project.Utils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.sustech.cs209a_project.pojo.RelationNode;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class RepoSearcher {
     static String token = "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";
 
     public static void main(String[] args) throws IOException, InterruptedException {
+//        getRepoStarTenToMAX();
+        getRelationship();
+
+    }
+
+    /**
+     * main function for get json of repos whose stars are from 10 to maximum
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void getRepoStarTenToMAX() throws IOException, InterruptedException {
         int[][] starBound = {{10, 40, 70, 100, 200, 400, 1000, 2000, 7000, 10000, 1000000},
                 {-1, 1, 5, 10, 10, 50, 100, 0, 0, 0, 0}};
         JSONArray storedJson = new JSONArray();
@@ -26,9 +47,161 @@ public class RepoSearcher {
                 storedJson = new JSONArray();
             }
         }
-//        String jsonStr = jsonArray.toJSONString();
-//        System.out.println(jsonStr);
     }
+
+    public static void getRelationship() throws IOException {
+        JSONArray jsonArray = JsonIO.readJSONArray("jsonTotal.json");
+        assert jsonArray != null;
+        System.out.printf("Number of repos: %d\n", jsonArray.size());
+
+        double step = 5000;
+        int times = (int) Math.ceil(jsonArray.size() / step);
+        for (int i = 0; i < times; i++) {
+            int begin = (int) step * i;
+            int end = (int) step * (i + 1);
+            JSONObject json = getRelationWithOthersFromJSONArray(jsonArray, begin, end);
+            JsonIO.saveJSON(json, String.format("relation%dto%d.json", begin, end));
+        }
+
+    }
+
+    public static JSONObject getRelationWithOthersFromJSONArray(JSONArray jsonArray, int begin, int end) throws IOException {
+        String urlKey = "languages_url";
+        double scale = 0.0001;
+
+        // get raw data
+        ArrayList<String> languageName = new ArrayList<>();
+        ArrayList<Double> nums = new ArrayList<>();
+        HashMap<String, HashSet<String>> rawLinks = new HashMap<>();
+
+        for (int i = begin; i < Math.min(end, jsonArray.size()); i++) {
+            JSONObject test = jsonArray.getJSONObject(i);
+            String url = jsonArray.getJSONObject(i).getString(urlKey);
+
+            // invoke api
+            boolean hasGet = false;
+            while (!hasGet) {
+                Connection.Response response = Jsoup.connect(url)
+                        .header("Authorization", String.format("token %s", token))
+                        .ignoreHttpErrors(true).ignoreContentType(true).execute();
+                JSONObject jsonRes = JSON.parseObject(response.body());
+                if (response.statusCode() == 200) {
+                    hasGet = true;
+                    double sum = 0;
+                    HashMap<String, Double> tmp = new HashMap<>();
+                    for (String key : jsonRes.keySet()) {
+                        double num = jsonRes.getDouble(key);
+                        tmp.put(key, num);
+                        sum += num;
+                    }
+                    // calc percent value of languages
+                    for (Iterator<String> it = tmp.keySet().iterator(); it.hasNext(); ) {
+                        String key = it.next();
+                        double ratio = tmp.get(key) / sum;
+                        if (ratio < 0.001) {
+                            it.remove();
+                            continue;
+                        }
+                        if (languageName.contains(key)) {
+                            int index = languageName.indexOf(key);
+                            nums.set(index, nums.get(index) + ratio);
+                        } else {
+                            languageName.add(key);
+                            nums.add(ratio);
+                        }
+                        if (key.equals("Java")) {
+                            it.remove();
+                        }
+                    }
+                    // get links of other languages
+                    for (String key : tmp.keySet()) {
+                        for (String key2 : tmp.keySet()) {
+                            if (key2.equals(key)) {
+                                continue;
+                            }
+                            if (!(rawLinks.containsKey(key) || rawLinks.containsKey(key2))) {
+                                rawLinks.put(key, new HashSet<>() {
+                                    {
+                                        add(key2);
+                                    }
+                                });
+                            } else {
+                                if (rawLinks.containsKey(key)) {
+                                    rawLinks.get(key).add(key2);
+                                } else {
+                                    rawLinks.get(key2).add(key);
+                                }
+                            }
+                        }
+                    }
+
+                } else {
+                    // rate is limited by github
+                    JSONObject rateJson = JSON.parseObject(Jsoup.connect("https://api.github.com/rate_limit")
+                            .header("Authorization", String.format("token %s", token))
+                            .ignoreContentType(true).execute().body());
+                    Instant epochSec = Instant.ofEpochSecond(rateJson.getJSONObject("rate").getLong("reset"));
+                    ZoneId zId = ZoneId.systemDefault();
+                    ZonedDateTime then = ZonedDateTime.ofInstant(epochSec, zId);
+                    ZonedDateTime now = ZonedDateTime.now();
+                    long diffMin = ChronoUnit.MINUTES.between(now, then);
+                    long diffSec = ChronoUnit.SECONDS.between(now, then);
+                    System.out.printf("Need to wait %d min\n (now %s)", diffMin, now);
+                    try {
+                        Thread.sleep(diffSec * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            if (i % 1000 == 0) {
+                System.out.printf("Finish %d", i);
+            }
+        }
+
+        // transfer to json with a certain format
+        JSONObject json = new JSONObject();
+        JSONArray nodes = new JSONArray();
+        JSONArray links = new JSONArray();
+        JSONArray categories = new JSONArray();
+
+        // init nodes and links with java
+        int javaId = languageName.indexOf("Java");
+        for (int i = 0; i < languageName.size(); i++) {
+            RelationNode relationNode = new RelationNode(i, languageName.get(i), nums.get(i), nums.get(i),
+                    i * 10, 0, 0);
+            nodes.add(JSON.toJSON(relationNode));
+            if (javaId != i) {
+                JSONObject link = new JSONObject();
+                link.put("source", javaId);
+                link.put("target", i);
+                links.add(link);
+            }
+
+        }
+        // init links
+        for (String key : rawLinks.keySet()) {
+            int keyId = languageName.indexOf(key);
+            for (String v : rawLinks.get(key)) {
+                JSONObject link = new JSONObject();
+                link.put("source", keyId);
+                link.put("target", languageName.indexOf(v));
+                links.add(link);
+            }
+        }
+        // init categories
+        JSONObject category = new JSONObject();
+        category.put("name", "Language");
+        categories.add(category);
+
+        // integrate
+        json.put("nodes", nodes);
+        json.put("links", links);
+        json.put("categories", categories);
+        return json;
+    }
+
 
     public static JSONArray getReposByStar(int starLow, int starUp, int step) throws IOException, InterruptedException {
         String api = "https://api.github.com/search/repositories";
@@ -42,25 +215,29 @@ public class RepoSearcher {
         while (ptr < starUp) {
             int page = 1;
             while (page <= maxPage) {
+                boolean hasGet = false;
                 String qURL = String.format(url, ptr, ptr + step - 1, page);
-                Connection.Response response = Jsoup.connect(qURL)
-                        .header("Authorization", String.format("token %s", token))
-                        .ignoreContentType(true).ignoreHttpErrors(true).execute();
-                JSONObject json = JSON.parseObject(response.body());
-                if (response.statusCode() == 200) {
-                    JSONArray jsonArray = json.getJSONArray("items");
-                    if (jsonArray.size() != 0) {
-                        jsonArrayTotal.addAll(jsonArray);
-                    } else {
-                        break;
-                    }
-                } else {
-                    System.out.println(json.getString("message"));
-                    JSONObject rateJson = JSON.parseObject(Jsoup.connect("https://api.github.com/rate_limit")
+                while (!hasGet) {
+                    Connection.Response response = Jsoup.connect(qURL)
                             .header("Authorization", String.format("token %s", token))
-                            .ignoreContentType(true).execute().body());
-                    System.out.println(rateJson.getJSONObject("resources").getString("search"));
-                    Thread.sleep(5000);
+                            .ignoreContentType(true).ignoreHttpErrors(true).execute();
+                    JSONObject json = JSON.parseObject(response.body());
+                    if (response.statusCode() == 200) {
+                        hasGet = true;
+                        JSONArray jsonArray = json.getJSONArray("items");
+                        if (jsonArray.size() != 0) {
+                            jsonArrayTotal.addAll(jsonArray);
+                        } else {
+                            break;
+                        }
+                    } else {
+                        System.out.println(json.getString("message"));
+                        JSONObject rateJson = JSON.parseObject(Jsoup.connect("https://api.github.com/rate_limit")
+                                .header("Authorization", String.format("token %s", token))
+                                .ignoreContentType(true).execute().body());
+                        System.out.println(rateJson.getJSONObject("resources").getString("search"));
+                        Thread.sleep(5000);
+                    }
                 }
                 page++;
             }
