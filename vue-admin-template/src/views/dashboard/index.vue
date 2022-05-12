587a03d42fff0bb317cd9fccf39a3e869812e075
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-12 23:54:45 +0800

    init template

---
+++ vue-admin-template/src/views/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard-container">
+    hi
+    <LanguageRanks ref="rank"></LanguageRanks>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import LanguageRanks from "@/components/LanguageRanks";
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  components: {
+    LanguageRanks
+  },
+  mounted() {
+    this.$refs.rank.initEcharts();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
