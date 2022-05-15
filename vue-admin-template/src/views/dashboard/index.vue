8deae92f1687c9e59275ec4e89eb2c25bb2e11c1
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-15 20:47:33 +0800

    finish dashboard

--- vue-admin-template/src/views/dashboard/index.vue
+++ vue-admin-template/src/views/dashboard/index.vue
@@ -4,17 +4,19 @@
     <el-row class="graph-container" :style="{height:rankHeight}" style="padding: 16px 16px;">
       <TotalRank ref="totalRank"></TotalRank>
     </el-row>
-    <el-row :gutter="40">
-      <el-col :span="12" class="graph-container" :style="{height:addRankHight,width:addRankWidth}" >
+    <el-row>
+      <el-col :span="11" class="graph-container" :style="{height:secondHeight}" >
         <LanguageRanks ref="rank" :hgt="rankhgt"></LanguageRanks>
       </el-col>
-      <el-col :span="12" class="graph-container">
-        <RelationshipJava ref="relation"></RelationshipJava>
+      <el-col :span="1" style="border: 1px solid transparent">
+      </el-col>
+      <el-col :span="12" class="graph-container" :style="{height:secondHeight}">
+        <MetricRadar ref="radar" :hgt="rankhgt"></MetricRadar>
       </el-col>
     </el-row>
 
 
-    <WordCloud ref="wordCloud"></WordCloud>
+<!--    <WordCloud ref="wordCloud"></WordCloud>-->
   </div>
 </template>
 
@@ -22,9 +24,9 @@
 import {mapGetters} from 'vuex'
 import LanguageRanks from "@/components/LanguageRanks";
 import RelationshipJava from "@/components/RelationshipJava";
-import WordCloud from "@/components/WordCloud";
 import PanelGroup from "@/views/dashboard/components/PanelGroup";
 import TotalRank from "@/views/dashboard/components/TotalRank";
+import MetricRadar from "@/views/dashboard/components/MetricRadar";
 
 export default {
   name: 'Dashboard',
@@ -38,26 +40,23 @@ export default {
     rankHeight() {
       return window.innerHeight * 0.4 + "px";
     },
-    addRankHight() {
+    secondHeight() {
       return window.innerHeight * 0.5 + "px";
-    },
-    addRankWidth() {
-      return window.innerWidth * 0.4 + "px";
     },
     rankhgt(){
       return window.innerHeight * 0.45 + "px";
     }
   },
   components: {
+    MetricRadar,
     TotalRank,
-    WordCloud,
     RelationshipJava,
     LanguageRanks,
     PanelGroup
   },
   mounted() {
     this.$refs.rank.draw();
-    this.$refs.relation.draw();
+    this.$refs.radar.draw();
     this.$refs.totalRank.draw();
   },
   watch: {}
