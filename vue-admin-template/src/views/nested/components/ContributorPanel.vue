e67165725cd3608bbb21d9bef8fb1eaa28ccf704
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-16 16:40:53 +0800

    add some svg and corresponding code

--- vue-admin-template/src/views/nested/components/ContributorPanel.vue
+++ vue-admin-template/src/views/nested/components/ContributorPanel.vue
@@ -7,28 +7,52 @@
       <el-col :span="10">
         <WorkTime ref="workTime" class="graph-container"></WorkTime>
       </el-col>
-      <el-col :span="4" >
-      <div style="float:right; position: relative; bottom: 50px; right:25px;" >
-        <el-row>
-          <el-image v-if="repo!=='Github'"
-            style="border-radius: 50%;"
-            :style="{width:radius, height:radius}"
-            :src="url"
-            fit="fit">
-          </el-image>
-          <svg-icon v-if="repo==='Github'" icon-class="github" :style="{width:radius, height:radius}" />
-        </el-row>
-        <el-row>
+      <el-col :span="4">
+        <div style="float:right; position: relative; bottom: 70px; right:15px;">
+          <el-row style="margin-bottom: 10px;">
+            <el-image v-if="repo!=='Github'"
+                      style="border-radius: 50%;"
+                      :style="{width:radius, height:radius}"
+                      :src="url"
+                      fit="fit">
+            </el-image>
+            <svg-icon v-if="repo==='Github'" icon-class="github" :style="{width:radius, height:radius}"/>
+          </el-row>
+          <el-row>
           <span class="card-repo-text">
-            {{repo}}
+            {{ repo }}
           </span>
-        </el-row>
-        <el-row>
+          </el-row>
+          <el-row>
           <span class="card-panel-text">
-            Owner: {{owner}}
+            Owner: {{ owner }}
           </span>
-        </el-row>
-      </div>
+          </el-row>
+          <el-row style="margin-top: 5px;">
+            <svg-icon icon-class="people" style="color: dodgerblue; font-size:20px;" ></svg-icon>
+            <span class="card-panel-text" style="margin-left: 5px">
+              <strong>{{contributors}}</strong>  contributors
+            </span>
+          </el-row>
+          <el-row style="margin-top: 5px;">
+            <svg-icon icon-class="comment_fill" style="color: green; font-size:20px;" ></svg-icon>
+            <span class="card-panel-text" style="margin-left: 5px">
+              <strong>{{comments}}</strong>  comments
+            </span>
+          </el-row>
+          <el-row style="margin-top: 5px;">
+            <svg-icon icon-class="release-fill" style="color: grey; font-size:20px;" ></svg-icon>
+            <span class="card-panel-text" style="margin-left: 5px">
+              <strong>{{releases}}</strong>  released
+            </span>
+          </el-row>
+          <el-row style="margin-top: 5px;">
+            <svg-icon icon-class="archive" style="color: red; font-size:20px;" ></svg-icon>
+            <span class="card-panel-text" style="margin-left: 5px">
+              <strong>{{archives}}</strong>  archived
+            </span>
+          </el-row>
+        </div>
       </el-col>
 
 
@@ -39,6 +63,7 @@
 <script>
 import WorkTime from "@/views/nested/components/WorkTime";
 import MultiMetric from "@/views/nested/components/MultiMetric";
+
 export default {
   name: "ContributorPanel",
   components: {MultiMetric, WorkTime},
@@ -46,12 +71,16 @@ export default {
     return {
       url: 'https://avatars.githubusercontent.com/u/15308811?v=4',
       owner: 'github',
-      repo: 'Github'
+      repo: 'Github',
+      contributors: 0,
+      comments: 0,
+      releases: 0,
+      archives: 0
     }
   },
-  computed:{
-    radius(){
-      return Math.min(window.innerWidth, window.innerHeight) *0.25 +"px";
+  computed: {
+    radius() {
+      return Math.min(window.innerWidth, window.innerHeight) * 0.3 + "px";
     }
   },
   mounted() {
@@ -62,16 +91,18 @@ export default {
 </script>
 
 <style scoped>
-.card-repo-text{
+.card-repo-text {
   font-size: 25px;
   margin-bottom: 12px;
 }
+
 .card-panel-text {
   line-height: 18px;
   color: rgba(0, 0, 0, 0.6);
   font-size: 16px;
   margin-bottom: 12px;
 }
+
 .graph-container {
   background-color: rgb(255, 255, 255);
   padding: 16px 16px;
