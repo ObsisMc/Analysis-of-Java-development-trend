81dda997c96ae1f979ee39929a79e250f2ec8c5c
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-16 15:20:00 +0800

    update worktime of third part

--- vue-admin-template/src/views/nested/components/ContributorPanel.vue
+++ vue-admin-template/src/views/nested/components/ContributorPanel.vue
@@ -1,34 +1,78 @@
 <template>
   <div>
     <el-row>
-      <el-col :span="8">
+      <el-col :span="10">
         hi
       </el-col>
-      <el-col :span="8">
-        hi
+      <el-col :span="10">
+        <WorkTime ref="workTime" class="graph-container"></WorkTime>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="4" >
+      <div style="float:right; position: relative; bottom: 50px; right:25px;" >
+        <el-row>
+          <el-image v-if="repo!=='Github'"
+            style="border-radius: 50%;"
+            :style="{width:radius, height:radius}"
+            :src="url"
+            fit="fit">
+          </el-image>
+          <svg-icon v-if="repo==='Github'" icon-class="github" :style="{width:radius, height:radius}" />
+        </el-row>
+        <el-row>
+          <span class="card-repo-text">
+            {{repo}}
+          </span>
+        </el-row>
+        <el-row>
+          <span class="card-panel-text">
+            Owner: {{owner}}
+          </span>
+        </el-row>
+      </div>
+      </el-col>
 
-        <el-image
-          style="width: 100px; height: 100px;border-radius: 50%;"
-          :src="url"
-          fit="fit"></el-image>
-      </el-col>
+
     </el-row>
   </div>
 </template>
 
 <script>
+import WorkTime from "@/views/nested/components/WorkTime";
 export default {
   name: "ContributorPanel",
+  components: {WorkTime},
   data() {
     return {
-      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+      url: 'https://avatars.githubusercontent.com/u/15308811?v=4',
+      owner: 'github',
+      repo: 'Github'
     }
+  },
+  computed:{
+    radius(){
+      return Math.min(window.innerWidth, window.innerHeight) *0.25 +"px";
+    }
+  },
+  mounted() {
+    this.$refs.workTime.draw();
   }
 }
 </script>
 
 <style scoped>
-
+.card-repo-text{
+  font-size: 25px;
+  margin-bottom: 12px;
+}
+.card-panel-text {
+  line-height: 18px;
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 16px;
+  margin-bottom: 12px;
+}
+.graph-container {
+  background-color: rgb(255, 255, 255);
+  padding: 16px 16px;
+  margin-bottom: 32px;
+}
 </style>
