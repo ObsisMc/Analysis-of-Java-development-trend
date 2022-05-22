b62e24c28f1a22560eaa1603ce8964a03d20a4ac
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-22 17:59:59 +0800

    finish CommitByTime.vue

--- vue-admin-template/src/views/nested/components/timeseries/CommitByTime.vue
+++ vue-admin-template/src/views/nested/components/timeseries/CommitByTime.vue
@@ -14,11 +14,13 @@ export default {
       defaultOption: null,
       option: null,
       chartDom: null,
-      myCharts: null
+      myChart: null
     }
   },
   methods: {
     draw() {
+      console.log(this.option.series.data);
+      console.log(this.option.xAxis.data);
       this.myChart.setOption(this.option);
     },
     getData(url) {
@@ -28,16 +30,15 @@ export default {
           url: url
         }
       }).then(response => {
-        var responseData = response.data;
-
-        let data = [];
-        let date = [];
-        for (let key in responseData) {
-          date.push(key);
-          data.push(responseData[key]);
-        }
-        this.data = data;
-        this.date = date;
+        // var responseData = eval("(" + response.data + ")");
+        // let data = [];
+        // let date = [];
+        // for (let i = 0; i < responseData.length; i++) {
+        //   data.push(responseData[i].count);
+        //   date.push(responseData[i].date);
+        // }
+        const date = ["2000-01-01", "2000-01-02"];
+        const data = [3, 5]
         this.option.xAxis.data = date;
         this.option.series.data = data;
         this.draw();
@@ -54,82 +55,39 @@ export default {
       this.chartDom = document.getElementById('commitByTime');
       this.myChart = this.$echarts.init(this.chartDom);
 
-      let base = +new Date(1968, 9, 3);
-      let oneDay = 24 * 3600 * 1000 * Math.random();
-      let date = [];
-      let data = [Math.random() * 300];
-      for (let i = 1; i < 20000; i++) {
-        var now = new Date((base += oneDay));
-        date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('-'));
-        data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
-      }
-
+      const date = ["2000-06-05", "2000-06-06"];
+      const data = [116, 100]
       this.defaultOption = {
-        tooltip: {
-          trigger: 'axis',
-          position: function (pt) {
-            return [pt[0], '10%'];
-          }
-        },
-        title: {
-          left: 'center',
-          text: 'Commits by time'
-        },
-        toolbox: {
-          feature: {
-            dataZoom: {
-              yAxisIndex: 'none'
-            },
-            restore: {},
-            saveAsImage: {}
-          }
-        },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: date
-        },
-        yAxis: {
-          type: 'value',
-          boundaryGap: [0, '100%']
-        },
-        dataZoom: [
+        // Make gradient line here
+        visualMap: [
           {
-            type: 'inside',
-            start: 0,
-            end: 10
-          },
-          {
-            start: 0,
-            end: 10
+            show: false,
+            type: 'continuous',
           }
         ],
-        series: [
+        title: [
           {
-            name: 'commits number',
+            left: 'center',
+            text: 'Gradient along the y axis'
+          }
+        ],
+        tooltip: {
+          trigger: 'axis'
+        },
+        xAxis: {
+          data: date
+        },
+        yAxis: [{}],
+        series:
+          {
             type: 'line',
-            symbol: 'none',
-            sampling: 'lttb',
-            itemStyle: {
-              color: 'rgb(255, 70, 131)'
-            },
-            areaStyle: {
-              color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                {
-                  offset: 0,
-                  color: 'rgb(255, 158, 68)'
-                },
-                {
-                  offset: 1,
-                  color: 'rgb(255, 70, 131)'
-                }
-              ])
-            },
+            showSymbol: false,
             data: data
           }
-        ]
+
       };
       this.option = this.defaultOption;
+      this.draw();
     }
   },
   mounted() {
@@ -137,7 +95,6 @@ export default {
       this.getData(url)
     })
     this.init();
-    this.draw();
   }
 }
 </script>
