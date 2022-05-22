db171efbbf800d2999b73949b7c3a102cabf91a3
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-05-22 17:34:37 +0800

    update SearchInput.vue

--- vue-admin-template/src/views/nested/components/SearchInput.vue
+++ vue-admin-template/src/views/nested/components/SearchInput.vue
@@ -43,7 +43,7 @@ export default {
       this.finished += 1;
       if (!state && !this.error) {
         this.$notify.error({
-          message: "Invalid URL or private repo"
+          message: "Invalid URL, private repo or rate limit"
         })
         this.error = !this.error;
       }
